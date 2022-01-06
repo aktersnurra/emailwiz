b188b2cb8b0f93fcdcac51aabe743c7fd8a5dbd4
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-06 17:45:21 +0100

    Update rm script for arch

--- mailrm.sh
+++ mailrm.sh
@@ -2,7 +2,7 @@
 
 # Purge all configs from a previous attempt at a mail server.
 # Doesn't delete mail or anything like that.
-apt purge dovecot-core spamassassin postfix spamc opendkim
+pacman -Rns postfix dovecot opendkim spamassassin pigeonhole
 
 # Some stragglers that often stay undeleted.
-rm -rf /etc/dovecot /var/lib/dovecot
+rm -rf /etc/dovecot /var/lib/dovecot /etc/postfix
